421783afd9de1f29790828c15796fbee2a1e4d4d
Sang Phan <plsang@nii.ac.jp>
2014-06-26 17:32:14 +0900

    support different types of dense trajectories, including improved trajectory

--- densetraj_extract_mbh.m
+++ densetraj_extract_mbh.m
@@ -1,18 +1,30 @@
-function [ X ] = densetraj_extract_mbh( video_file, gap, start_frame, end_frame)
+function [ X ] = densetraj_extract_mbh( video_file, dt_type, start_frame, end_frame)
 %EXTRACT_FEATURES Summary of this function goes here
 %   Detailed explanation goes here
+	
+	% History:
+	% Jun 26: removed gap, set to defaul 1; added dense trajectory type
+	
+	% dt_type: 'dt', 'idt';
 	
 	if ~exist('gap', 'var'),
 		gap = 1;
 	end
 	
-	
-    densetraj = '/net/per900a/raid0/plsang/tools/dense_trajectory_release/release/DenseTrack_MBH';
+	switch dt_type,
+		case 'dt'
+			densetraj = '/net/per900a/raid0/plsang/tools/dense_trajectory_release/release/DenseTrack_MBH';
+		case 'idt'
+			densetraj = '/net/per900a/raid0/plsang/tools/improved_trajectory_release/release/DenseTrackStab_MBH';
+		otherwise
+			error('Unsupported Dense Trajectory Type\n');
+	end
+    
     % Set up the mpeg audio decode command as a readable stream
-    cmd = [densetraj, ' ', video_file, ' -G ', num2str(gap)];
+    cmd = [densetraj, ' ', video_file];
 	
 	if exist('start_frame', 'var') && exist('end_frame', 'var'),
-		cmd = [densetraj, ' ', video_file, ' -S ', num2str(start_frame), ' -E ', num2str(end_frame), ' -G ', num2str(gap)];
+		cmd = [densetraj, ' ', video_file, ' -S ', num2str(start_frame), ' -E ', num2str(end_frame)];
 	end
 	
 	feat_dim = 192;
