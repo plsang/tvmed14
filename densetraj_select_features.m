f5993779a889e628316e6bb6e0d50df72e5440fa
Sang Phan <plsang@nii.ac.jp>
2014-06-26 17:57:32 +0900

    modified select features to support multiple traj types

--- densetraj_select_features.m
+++ densetraj_select_features.m
@@ -1,34 +1,49 @@
-function [ feats ] = densetraj_select_features( descriptor, max_features )
+function [ feats ] = densetraj_select_features( dt_type, max_features )
 %SELECT_FEATURES Summary of this function goes here
 %   Detailed explanation goes here
 
-	%% event_set = 1: 10ex, 2:100Ex, 3: 130Ex
-	
     % parameters
 	if ~exist('max_features', 'var'),
 		max_features = 1000000;
 	end
+	
+	%% event_set = 1: 10ex, 2:100Ex, 3: 130Ex
+	configs = set_global_config();
+	logfile = sprintf('%s/%s.log', configs.logdir, mfilename);
+	msg = sprintf('Start running %s(%s, %d)', mfilename, dt_type, max_features);
+	logmsg(logfile, msg);
+	
+	tic;
+	
     video_sampling_rate = 1;
 	sample_length = 120; % frames
 	 
-	f_metadata = '/net/per610a/export/das11f/plsang/trecvidmed13/metadata/common/metadata.mat';
+	 %% TODO: using unified metadata
+	f_metadata = '/net/per610a/export/das11f/plsang/trecvidmed13/metadata/common/metadata_devel.mat';
 	fprintf('Loading metadata...\n');
 	metadata_ = load(f_metadata, 'metadata');
 	metadata = metadata_.metadata;
 	
     video_dir = '/net/per610a/export/das11f/plsang/dataset/MED2013/LDCDIST-RSZ';
 	
-	csv_dir = '/net/per610a/export/das11f/plsang/dataset/MED2013/MEDDATA/databases';
-	eventbg_csv = 'EVENTS-BG_20130405_ClipMD.csv';
-	f_eventvideo_csv = 'EVENTS-130Ex_20130405_ClipMD.csv';
+	% csv_dir = '/net/per610a/export/das11f/plsang/dataset/MED2013/MEDDATA/databases';
+	% eventbg_csv = 'EVENTS-BG_20130405_ClipMD.csv';
+	% f_eventvideo_csv = 'EVENTS-130Ex_20130405_ClipMD.csv';
 
-	f_eventvideo_csv = fullfile(csv_dir,f_eventvideo_csv);	
-	f_eventbg_csv = fullfile(csv_dir, eventbg_csv);
+	% f_eventvideo_csv = fullfile(csv_dir,f_eventvideo_csv);	
+	% f_eventbg_csv = fullfile(csv_dir, eventbg_csv);
 	
-	list_eventvideo = load_video_list(f_eventvideo_csv);
-	list_bgvideo = load_video_list(f_eventbg_csv);
+	% list_eventvideo = load_video_list(f_eventvideo_csv);
+	% list_bgvideo = load_video_list(f_eventbg_csv);
 	
-	list_video = [list_eventvideo, list_bgvideo];
+	% list_video = [list_eventvideo, list_bgvideo];
+	
+	fprintf('Loading metadata...\n');
+	medmd_file = '/net/per610a/export/das11f/plsang/trecvidmed13/metadata/medmd.mat';
+	load(medmd_file, 'MEDMD'); 
+	
+	clips = MEDMD.EventBG.default.clips;
+	list_video = unique(clips);	% 4992 clips
 	
 	num_selected_videos = ceil(video_sampling_rate * length( list_video ));
 	rand_index = randperm(length(list_video));
@@ -57,7 +72,7 @@ function [ feats ] = densetraj_select_features( descriptor, max_features )
 		
 		fprintf('\n--- [%d/%d] Computing features for video %s ...\n', ii, length(selected_videos), video_name);
 		
-        feat = densetraj_extract_mbh(video_file, 5, start_frame, end_frame);
+        feat = densetraj_extract_mbh(video_file, dt_type, start_frame, end_frame);
         
         if size(feat, 2) > max_features_per_video,
             feats{ii} = vl_colsubset(feat, max_features_per_video);
@@ -74,7 +89,7 @@ function [ feats ] = densetraj_select_features( descriptor, max_features )
          feats = vl_colsubset(feats, max_features);
     end
 
-	output_file = sprintf('/net/per610a/export/das11f/plsang/trecvidmed13/feature/bow.codebook.devel/densetrajectory.%s/data/selected_feats_%d_%d.mat', descriptor, max_features);
+	output_file = sprintf('/net/per610a/export/das11f/plsang/trecvidmed13/feature/bow.codebook.devel/densetraj.mbh.%s/data/selected_feats_%d_%d.mat', dt_type, max_features);
 	output_dir = fileparts(output_file);
 	if ~exist(output_dir, 'file'),
 		cmd = sprintf('mkdir -p %s', output_dir);
@@ -83,34 +98,12 @@ function [ feats ] = densetraj_select_features( descriptor, max_features )
 	
 	fprintf('Saving selected features to [%s]...\n', output_file);
     save(output_file, 'feats', '-v7.3');
-    
+   
+    elapsed = toc;
+	elapsed_str = datestr(datenum(0,0,0,0,0,elapsed),'HH:MM:SS');
+	
+	msg = sprintf('Finish running %s(%s, %d). Elapsed time: %s', mfilename, dt_type, max_features, elapsed_str);
+	logmsg(logfile, msg);
 end
 
 
-function list_video = load_video_list(csv_file),
-
-	fprintf('--- Loading video from file %s ...\n', csv_file);
-	
-	fh = fopen(csv_file);
-	
-	cvs_infos = textscan(fh, '%s %s %s %s %s', 'delimiter', ',');
-	clip_ids = cvs_infos{1};
-	fclose(fh);
-		
-	clip_prefix = 'HVC';
-	
-	list_video = {};
-	for ii = 2:length(clip_ids),
-		if ~mod(ii, 1000),
-			fprintf('%d ', ii);
-		end
-		
-		clip_id = str2num(strtrim(strrep(clip_ids{ii}, '"', '')));
-	
-		clip_name = sprintf('%s%06d', clip_prefix, clip_id);
-		
-		list_video{end+1} = clip_name;
-	end
-	
-	fprintf('\n');
-end
