92522b45bd0e3c4eca3d261dbea53f74d234804d
Sang Phan <plsang@nii.ac.jp>
2014-06-30 16:03:10 +0900

    added feature space for sift descriptor

--- sift_extract_features.m
+++ sift_extract_features.m
@@ -1,17 +1,26 @@
-function [frames, descrs] = sift_extract_features( im, sift_algo, param )
+function [frames, descrs] = sift_extract_features( img_path, sift_algo, param )
 	%param
 	% sift_algo is dsift --> param is nSize
 	% sift_algo is phow --> param is color type for vl_phow: GRAY (PHOW-gray), RGB, HSV, and OPPONENT (PHOW-color).
-
+	% sift_algo is covdet --> param is hessian
+	% sift_algo is fspace --> param is hesaff, note: im is image path
+	
 	%% SIFT parameters
 	switch (sift_algo),
 		case 'dsift'
+			
 			if exist('param', 'var'),
 				obj.nSize = param;
 			else
 				obj.nSize = 6;
 			end
-			[frames, descrs] = vl_dsift(single(rgb2gray(im)), 'step', obj.nSize);
+			try
+				im = imread(img_path);
+				[frames, descrs] = vl_dsift(single(rgb2gray(im)), 'step', obj.nSize);
+			catch
+				frames = [];
+				descrs = [];
+			end
 			
 		case 'covdet'
 			if exist('param', 'var'),
@@ -19,7 +28,15 @@ function [frames, descrs] = sift_extract_features( im, sift_algo, param )
 			else
 				obj.method = 'dog';
 			end
-			[frames, descrs] = vl_covdet(single(rgb2gray(im)), 'method', obj.method);
+			
+			try
+				im = imread(img_path);
+				[frames, descrs] = vl_covdet(single(rgb2gray(im)), 'method', obj.method);
+			catch
+				frames = [];
+				descrs = [];
+			end
+			
 				
 		case 'phow'
             obj.verbose = false;
@@ -36,13 +53,37 @@ function [frames, descrs] = sift_extract_features( im, sift_algo, param )
             obj.magnif = 6;
             obj.float_descriptors = false;
 			
-			im = standardizeImage(im); 
-			[frames, descrs] = vl_phow(im, 'Verbose', obj.verbose, ...
-				'Sizes', obj.sizes, 'Fast', obj.fast, 'step', obj.step, ...
-				'Color', obj.color, 'ContrastThreshold', obj.contrast_threshold, ...
-				'WindowSize', obj.window_size, 'Magnif', obj.magnif, ...
-				'FloatDescriptors', obj.float_descriptors);
-				
+			try
+				im = imread(img_path);
+				im = standardizeImage(im); 
+				[frames, descrs] = vl_phow(im, 'Verbose', obj.verbose, ...
+					'Sizes', obj.sizes, 'Fast', obj.fast, 'step', obj.step, ...
+					'Color', obj.color, 'ContrastThreshold', obj.contrast_threshold, ...
+					'WindowSize', obj.window_size, 'Magnif', obj.magnif, ...
+					'FloatDescriptors', obj.float_descriptors);
+			catch
+				frames = [];
+				descrs = [];
+			end
+			
+		case 'fspace' %param = hesaff
+			tmpdir = '/net/per900a/raid0/plsang/tmp/sift';
+			fspace_bin = '/net/per900a/raid0/plsang/tools/featurespace/compute_descriptors_64bit.ln';
+			[~, fname, fext] = fileparts(img_path);
+			[~, tmpname] = fileparts(tempname);
+			tmp_feat_file = sprintf('%s/%s.%s.feat', tmpdir, fname, tmpname);
+			%<duong dan den file anh input>
+			cmd = sprintf('%s -%s -sift -noangle -i %s -o1 %s', fspace_bin, param, img_path, tmp_feat_file);
+			system(cmd);
+			[frames, descrs] = load_fspace_feature(tmp_feat_file);
+			
+			%%% applying root sift
+			if ~isempty(descrs),
+				sift = double(descrs);
+				descrs = single(sqrt(sift./repmat(sum(sift), 128, 1)));
+			end
+
+			delete(tmp_feat_file);
 		otherwise
 			error('Unknow sift algorithm!!!\n');
 	end
