5674ead0e3bb858d6e3a5186d905471a94962d85
Sang Phan <plsang@nii.ac.jp>
2014-07-04 10:36:45 +0900

    added global config and COMMAND for running

---
+++ set_global_config.m
@@ -0,0 +1,4 @@
+function configs = set_global_config()
+	configs.logdir = '/net/per610a/export/das11f/plsang/codes/kaori-secode-med14/log';
+	configs.sgelogdir = '/net/per610a/export/das11f/plsang/codes/kaori-secode-med14/sgelog';
+end
