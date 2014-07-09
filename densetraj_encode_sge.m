9e07cf89e7c14c6e74172546039e33b8cadbfae0
Sang Phan <plsang@nii.ac.jp>
2014-07-09 23:07:31 +0900

    calculate hoghof and mbh at the same time

--- densetraj_encode_sge.m
+++ densetraj_encode_sge.m
@@ -1,4 +1,4 @@
-function [ output_args ] = densetraj_encode_sge( dt_type, exp_ann, start_seg, end_seg )
+function [ output_args ] = densetraj_encode_sge( descriptor, exp_ann, start_seg, end_seg )
 %ENCODE Summary of this function goes here
 %   Detailed explanation goes here
 %% kf_dir_name: name of keyframe folder, e.g. keyframe-60 for segment length of 60s   
@@ -24,7 +24,8 @@ function [ output_args ] = densetraj_encode_sge( dt_type, exp_ann, start_seg, en
 	medmd_file = '/net/per610a/export/das11f/plsang/trecvidmed13/metadata/medmd.mat';
 	load(medmd_file, 'MEDMD'); 
 	
-	train_clips = [MEDMD.EventKit.EK10Ex.clips, MEDMD.EventKit.EK100Ex.clips, MEDMD.EventKit.EK130Ex.clips, MEDMD.EventBG.default.clips];
+	%train_clips = [MEDMD.EventKit.EK10Ex.clips, MEDMD.EventKit.EK100Ex.clips, MEDMD.EventKit.EK130Ex.clips, MEDMD.EventBG.default.clips];
+	train_clips = [MEDMD.EventKit.EK130Ex.clips, MEDMD.EventBG.default.clips];
 	train_clips = unique(train_clips);
 	
 	test_clips = MEDMD.RefTest.KINDREDTEST.clips;
@@ -33,9 +34,9 @@ function [ output_args ] = densetraj_encode_sge( dt_type, exp_ann, start_seg, en
 	
 	codebook_gmm_size = 256;
     
-	feature_ext_fc = sprintf('densetraj.mbh.%s.cb%d.fc', dt_type, codebook_gmm_size);
+	feature_ext_fc = sprintf('idensetraj.%s.cb%d.fc', descriptor, codebook_gmm_size);
 	if dimred > 0,
-		feature_ext_fc = sprintf('densetraj.mbh.%s.cb%d.fc.pca', dt_type, codebook_gmm_size);
+		feature_ext_fc = sprintf('idensetraj.%s.cb%d.fc.pca', descriptor, codebook_gmm_size);
 	end
 
     output_dir_fc = sprintf('%s/%s/%s', fea_dir, exp_ann, feature_ext_fc);
@@ -46,17 +47,28 @@ function [ output_args ] = densetraj_encode_sge( dt_type, exp_ann, start_seg, en
 	
 	% loading gmm codebook
 	
-	codebook_gmm_file = sprintf('/net/per610a/export/das11f/plsang/trecvidmed13/feature/bow.codebook.devel/densetraj.mbh.%s/data/codebook.gmm.%d.mat', dt_type, codebook_gmm_size);
+	codebook_hoghof_file = sprintf('/net/per610a/export/das11f/plsang/trecvidmed13/feature/bow.codebook.devel/idensetraj.hoghof/data/codebook.gmm.%d.mat', codebook_gmm_size);
+	codebook_mbh_file = sprintf('/net/per610a/export/das11f/plsang/trecvidmed13/feature/bow.codebook.devel/idensetraj.mbh/data/codebook.gmm.%d.mat', codebook_gmm_size);
 	low_proj = [];
 	
-	if dimred > 0,
-		codebook_gmm_file = sprintf('/net/per610a/export/das11f/plsang/trecvidmed13/feature/bow.codebook.devel/densetraj.mbh.%s/data/codebook.gmm.%d.%d.mat', dt_type, codebook_gmm_size, dimred);
-		low_proj_file = sprintf('/net/per610a/export/das11f/plsang/trecvidmed13/feature/bow.codebook.devel/densetraj.mbh.%s/data/lowproj.%d.%d.mat', dt_type, dimred, 192);
-		low_proj_ = load(low_proj_file, 'low_proj');
-		low_proj = low_proj_.low_proj;
-	end
-    codebook_gmm_ = load(codebook_gmm_file, 'codebook');
-    codebook_gmm = codebook_gmm_.codebook;
+	
+	codebook_hoghof_file = sprintf('/net/per610a/export/das11f/plsang/trecvidmed13/feature/bow.codebook.devel/idensetraj.hoghof/data/codebook.gmm.%d.%d.mat', codebook_gmm_size, dimred);
+	low_proj_hoghof_file = sprintf('/net/per610a/export/das11f/plsang/trecvidmed13/feature/bow.codebook.devel/idensetraj.hoghof/data/lowproj.%d.%d.mat', descriptor, dimred, 204);
+	
+	codebook_mbh_file = sprintf('/net/per610a/export/das11f/plsang/trecvidmed13/feature/bow.codebook.devel/idensetraj.mbh/data/codebook.gmm.%d.%d.mat', codebook_gmm_size, dimred);
+	low_proj_mbh_file = sprintf('/net/per610a/export/das11f/plsang/trecvidmed13/feature/bow.codebook.devel/idensetraj.mbh/data/lowproj.%d.%d.mat', descriptor, dimred, 192);
+
+	codebook_hoghof_ = load(codebook_hoghof_file, 'codebook');
+    codebook_hoghof = codebook_hoghof_.codebook;
+	
+	codebook_mbh_ = load(codebook_mbh_file, 'codebook');
+    codebook_mbh = codebook_mbh_.codebook;
+	
+	low_proj_hoghof_ = load(low_proj_hoghof_file, 'low_proj');
+	low_proj_hoghof = low_proj_hoghof_.low_proj;
+	
+	low_proj_mbh_ = load(low_proj_mbh_file, 'low_proj');
+	low_proj_mbh = low_proj_mbh_.low_proj;
 	
     if start_seg < 1,
         start_seg = 1;
@@ -72,7 +84,8 @@ function [ output_args ] = densetraj_encode_sge( dt_type, exp_ann, start_seg, en
 	
         video_file = fullfile(video_dir, metadata.(video_id).ldc_pat);
 		
-		output_fc_file = sprintf('%s/%s/%s.mat', output_dir_fc, fileparts(metadata.(video_id).ldc_pat), video_id);
+		output_hoghof_file = sprintf('%s/%s/%s.hoghof.mat', output_dir_fc, fileparts(metadata.(video_id).ldc_pat), video_id);
+		output_mbh_file = sprintf('%s/%s/%s.mbh.mat', output_dir_fc, fileparts(metadata.(video_id).ldc_pat), video_id);
 		
         if exist(output_fc_file, 'file') ,
             fprintf('File [%s] already exist. Skipped!!\n', output_fc_file);
@@ -81,9 +94,10 @@ function [ output_args ] = densetraj_encode_sge( dt_type, exp_ann, start_seg, en
 		
         fprintf(' [%d --> %d --> %d] Extracting & Encoding for [%s]...\n', start_seg, ii, end_seg, video_id);
         
-        code = densetraj_extract_and_encode(dt_type, video_file, codebook_gmm, low_proj); %important
+        [code_hoghof, code_mbh] = densetraj_extract_and_encode_hoghofmbh(video_file, codebook_hoghof, low_proj_hoghof, codebook_mbh, low_proj_mbh); %important
         
-		par_save(output_fc_file, code); 	
+		par_save(output_hoghof_file, code_hoghof); 	
+		par_save(output_mbh_file, code_mbh); 	
 
     end
     
