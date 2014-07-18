e9192aa8c234e8b60962476dfa0734a6514b1826
plsang <plsang@nii.ac.jp>
2014-07-18 11:54:30 +0900

    learn spatial model + normalizer

--- sift_sfv_learn_appearance_model.m
+++ sift_sfv_learn_appearance_model.m
@@ -2,9 +2,18 @@ function sift_sfv_learn_appearance_model(proj_dir, feat_pat);
 	
 	set_env;
 	
-	feat_dim_proj = 80;
-	appearance_components = 256;
+	parms.feat_dim_proj = 80;
+	parms.appearance_components = 256;
+	parms.spatial_components = '1';
 	
+	d = 2;
+	D = parms.feat_dim_proj;
+	C = str2num(parms.spatial_components);
+	K = parms.appearance_components;
+	
+	parms.imagevec_function = @get_asfv;
+    parms.imagevec_dim = K * (1 + 2 * D + C * (1 + 2 * d));
+			
 	if ~exist('num_features', 'var'),
 		num_features = 1000000;
 	end
@@ -19,21 +28,46 @@ function sift_sfv_learn_appearance_model(proj_dir, feat_pat);
 	fprintf('Loading selected features <%s>...\n', f_selected_feats);
 	load(f_selected_feats, 'feats');
 	
+	f_selected_positions = sprintf('%s/feature/bow.codebook.devel/%s/data/selected_positions_%d.mat', ...
+		proj_dir, feat_pat, num_features);
+	
+	if ~exist(f_selected_positions, 'file'),
+		error('File %s not found!\n', f_selected_positions);
+	end
+	
+	fprintf('Loading selected features <%s>...\n', f_selected_positions);
+	load(f_selected_positions, 'positions');
+	
+	
 	appearance_subspace = sift_sfv_get_pca(feats);
 	
-	appearance_projected = sift_sfv_pca_project(feats, appearance_subspace, feat_dim_proj);
-	
+	parms.appearance_projected_filename = sprintf('%s/feature/bow.codebook.devel/%s/data/sfv_appearance_projected_%d.mat', ...
+		proj_dir, feat_pat, parms.appearance_components);
+		
 	parms.appearance_model_filename = sprintf('%s/feature/bow.codebook.devel/%s/data/sfv_appearance_model_%d.mat', ...
-		proj_dir, feat_pat, appearance_components);
+		proj_dir, feat_pat, parms.appearance_components);
 	
 	parms.spatial_model_filename = sprintf('%s/feature/bow.codebook.devel/%s/data/sfv_spatial_model_%d.mat', ...
-		proj_dir, feat_pat, appearance_components);
+		proj_dir, feat_pat, parms.appearance_components);
+	
+	parms.normalizer_filename = sprintf('%s/feature/bow.codebook.devel/%s/data/sfv_normalizer_%d.mat', ...
+		proj_dir, feat_pat, parms.appearance_components);	
 		
+	if ~exist(parms.appearance_projected_filename,'file'),
+		parms.appearance_projected = sift_sfv_pca_project(feats, appearance_subspace, parms.feat_dim_proj);
+		save(parms.appearance_projected_filename,'-struct','parms','appearance_projected');
+	else
+		fprintf('Loading cached normalizer from %s\n', parms.appearance_projected_filename);
+		tmp = load(parms.appearance_projected_filename);
+		parms.appearance_projected = tmp.appearance_projected;
+		clear tmp;
+	end
+	
 	% learn appearance_model (or gmm codebook)
 	if ~exist(parms.appearance_model_filename,'file'),
-		[w, mu, sigma] = yael_gmm(single(appearance_projected), appearance_components, ...
+		[w, mu, sigma] = yael_gmm(single(parms.appearance_projected), parms.appearance_components, ...
 										  'redo', 10, ...
-										  'niter', 20, ...
+										  'niter', 100, ...
 										  'seed', now);
 		
 		parms.appearance_model.mix = w';
@@ -42,14 +76,38 @@ function sift_sfv_learn_appearance_model(proj_dir, feat_pat);
 		parms.appearance_model.W = zeros(size(mu,1),0,size(mu,2));
 		clear w; clear mu; clear sigma;
 		save(parms.appearance_model_filename,'-struct','parms','appearance_model');
+	else
+		fprintf('Loading cached appearance model from <%s>\n', parms.appearance_model_filename);
+        tmp = load(parms.appearance_model_filename);
+        parms.appearance_model = tmp.appearance_model;
+        clear tmp;
 	end
 	
 	% set spatial model
 	if ~exist(parms.spatial_model_filename,'file'),
-		parms.spatial_components = 1;
 		parms.spatial_model = set_spatial_model(parms);
 		save(parms.spatial_model_filename,'-struct','parms','spatial_model');
+	else
+		fprintf('Loading cached spatial model from %s\n', parms.spatial_model_filename);
+        tmp = load(parms.spatial_model_filename);
+        parms.spatial_model = tmp.spatial_model;
+        clear tmp;
 	end
 	
+	% learn normalizer
+	
+	if (exist(parms.normalizer_filename,'file'))
+		fprintf('Loading cached normalizer from %s\n', parms.normalizer_filename);
+		tmp = load(parms.normalizer_filename);
+		parms.normalizer = tmp.normalizer;
+		clear tmp;
+	else       
+		%parms = sample_training_features(parms);               
+		parms.normalizer = [];
+		parms.normalizer = parms.imagevec_function(positions, parms.appearance_projected, parms.appearance_model, parms.spatial_model, parms.normalizer);
+		parms.normalizer.additive = -parms.normalizer.additive; % we need to subtract the mean vector
+		parms.normalizer = parms.imagevec_function(positions, parms.appearance_projected, parms.appearance_model, parms.spatial_model, parms.normalizer);
+		save(parms.normalizer_filename,'-struct','parms','normalizer');
+	end
 	
 end
