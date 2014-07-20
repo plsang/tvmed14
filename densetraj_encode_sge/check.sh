3c68f9b17412bed191b03c38640c9552120532b8
plsang <plsang@nii.ac.jp>
2014-07-20 15:35:42 +0900

    remove log dir

--- densetraj_encode_sge/check.sh
+++
@@ -1,1 +0,0 @@
-qsub -e /net/per610a/export/das11f/plsang/codes/kaori-secode-med14.1/densetraj_encode_sge/error.log -o /net/per610a/export/das11f/plsang/codes/kaori-secode-med14.1/densetraj_encode_sge/output.log /net/per610a/export/das11f/plsang/codes/kaori-secode-med14.1/densetraj_encode_sge.sh idt video-bg 1 1
